0b39b675b6b5379eea89d58ff174dcbd921cb0c5
gsaini <46835538+gsaini26@users.noreply.github.com>
2022-08-29 09:26:43 -0700

    updated footer and added sound on key or click
    updated footer in the index.html and updated index.js to make a sound whenever a key or letter is pressed using event listeners for key presses and clicks.

--- index.js
+++ index.js
@@ -1,22 +1,82 @@
+/*document.querySelector("button").addEventListener("click", handleClick);*/
 
-/*document.querySelector("button").addEventListener("click", handleClick);
-
-function handleClick() {
+/*function handleClick() {
     alert("I got clicked!");
 }*/
 
 /*using anonymous function does the same thing */
 
-/* Do something when any button gets clicked */
 
-var drumList= document.querySelectorAll(".drum");
+/* Do something when any key or button gets clicked */
+
+function sounds(key) {
+
+    switch (key) {
+        case "w":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+
+        case "a":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+
+        case "s":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
 
 
-for (i=0; i<=drumList.length; i++) {
-    drumList[i].addEventListener("click", function() {
-        this.style.color='white';
+        case "d":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+        case "j":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+
+        case "k":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+
+        case "l":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+        default:
+            console.log(key);
+    }
+
+}
+
+/* select the drum class items and add to an array*/
+var drumList = document.querySelectorAll(".drum");
+
+/* Listen for a click of any of the buttons (indicated by the class drum) */
+for (i = 0; i <= drumList.length; i++) {
+    drumList[i].addEventListener("click", event => {
+     
+        /* if one of the buttons is clicked, send the innerHTML, 
+        which is the letter that was clicked (see the index.html)
+        to the sounds function that completes an action for the letter
+        clicked*/
+        
+        sounds(event.target.innerHTML);
+    
     });
+
+    /* listen for a key that is pressed rather than a button clicked
+    and have the sounds function complete an action based on 
+    which key was pressed */
+    
+    drumList[i].addEventListener("keydown", event => {
+        sounds(event.key);
+});
+
 };
 
-/*var audio=new Audio("sounds/snare.mp3");
-audio.play();*/
